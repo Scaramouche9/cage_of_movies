be9564c2a4a0306c6904e4af17561fcfa200fc1a
Emily Davies <85586446+Scaramouche9@users.noreply.github.com>
2021-12-16 14:07:09 -0500

    Update server.js
    Added server.js file

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-// import http module and listen for requests on port 8080
-// iport url module at top 
 
-const http = require('http');
+// calls needed modules
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  let addr = request.url,
+    q = url.parse(addr, true);
+  // filePath?
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\n Timestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+    console.log(err);
+      } else {
+    console.log('Added to log');
+    }
+  });
+  
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+  
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
 }).listen(8080);
+
