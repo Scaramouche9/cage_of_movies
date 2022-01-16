b09a9af425096d381ec3c8f7a105fbacaf1cb2b9
Emily Davies <85586446+Scaramouche9@users.noreply.github.com>
2022-01-16 18:53:11 -0500

    Added data validation
    In index.js, added data validation to endpoints for adding and updating users

--- index.js
+++ index.js
@@ -128,34 +128,38 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
 // POST add a user
-  /* Expected JSON response format:
-  {
-    ID: Integer,
-    Username: String,
-    Password: String,
-    Email: String,
-    Birthday: Date
-  } */
-app.post('/users', (req, res) => {
-  Users.findOne({ Username: req.body.Username })
+app.post('/users', [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username contains non aplhanumeric characters - not allowed').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Birthday', 'Birthday must be in format "YYYY-MM-DD"').isDate({format: 'YYYY-MM-DD'})
+  ], (req, res) => {
+    // check the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
+    }
   let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username }) // search to see if username already exists
     .then((user) => {
       if (user) {
         return res.status(400).send(req.body.Username + ' already exists');
       } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) => { res.status(201).json(user) })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
-          })
+        Users.create({
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }).then(
+          (user) => { res.status(201).json(user) })
+          .catch(
+            (error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            })
       }
     })
     .catch((error) => {
@@ -181,25 +185,30 @@ app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { sess
 });
 
 // PUT Update a user's info, by username
-/* Expected JSON response format:
-{
-  Username: String,
-  (required)
-  Password: String,
-  (required)
-  Email: String,
-  (required)
-  Birthday: Date
-}*/
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, {
-    $set:
-    {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
+app.put('/users/:Username', [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username contains non aplhanumeric characters - not allowed').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Birthday', 'Birthday must be in format "YYYY-MM-DD"').isDate({format: 'YYYY-MM-DD'})
+  ],
+  passport.authenticate('jwt', { session: false }), // BELONGS HERE?
+  (req, res) => {
+      // check the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
     }
+      Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $set:
+        {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }
   },
     { new: true }, // This line makes sure that the updated document is returned
     (err, updatedUser) => {
